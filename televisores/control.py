fd07aee1dce95a9b6dbb1686abfcad4623372fab
Andrés Pérez Cano <aperezcan@unal.edu.co>
2024-02-25 01:41:16 -0500

    1 version

---
+++ televisores/control.py
@@ -0,0 +1,46 @@
+class Control:
+    def __init__(self):
+        self._tv = None
+    
+    def turnOn(self):
+        if self._tv != None:
+            self._tv.turnOn()
+
+    def turnOff(self):
+         if self._tv != None:
+            self._tv.turnOff()
+
+    def canalUp(self):
+        if self._tv != None:
+            self._tv.canalUp()
+
+    def canalDown(self):
+        if self._tv != None:
+            self._tv.canalDown()
+
+    def volumenUp(self):
+        if self._tv != None:
+            self._tv.volumenUp()
+
+    def volumenDown(self):
+        if self._tv != None:
+            self._tv.volumenDown()
+
+    def setCanal(self, canal):
+        if self._tv != None:
+            self._tv.setCanal(canal)
+
+    def setVolumen(self, volumen):
+        if self._tv != None:
+            self._tv.setVolumen(volumen)
+    
+    def enlazar(self, tv):
+        self._tv = tv
+        if self._tv != None:
+            self._tv.control = self
+
+    def getTv(self):
+        return self._tv
+
+    def setTv(self, tv):
+        self._tv = tv
